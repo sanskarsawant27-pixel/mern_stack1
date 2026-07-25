22b9bfd0198d5cd87adde7b2949e0423b112e202
sanskarsawant27-pixel <sanskarsawant27@gmail.com>
2026-07-25 11:10:20 +0530

    Practical 2 folder with files

---
+++ Practical 2/array_object.js
@@ -0,0 +1,16 @@
+const Fruit = ['Apple','Banana','Orange'];
+console.log (Fruit[0]);
+Fruit.push('Mango');
+Fruit.unshift('kiwi');
+Fruit.pop();
+const total = Fruit.length;
+console.log(total);
+
+// Object
+const user = {
+    name :"HET",
+    age :"22",
+    isadmin :true};
+console.log(user.name);
+console.log(user['age']);
+
---
+++ Practical 2/function_para.js
@@ -0,0 +1,7 @@
+function calTotal(electronicsPrices,tax){
+    const total = price+tax;
+    return total;
+}
+const finalPrice = calTotal(100,50);
+console.log(finalPrice);
+
